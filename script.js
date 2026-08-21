b9612b1122fc2166e5bd5b02485ff152b0382c5e
SHARSHA-tech <harshashendge@gmail.com>
2026-08-21 14:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function insert_Row() {
     //Write your code here
   let body = document.getElementById("sampleTable");
-	document.createElement("tr");
+	let newRow = document.createElement("tr");
 	body.insertBefore("newRow",body.firstChild);
 	document.createElement("td");
     let cell1 = document.createElement("td");
 	cell1.textContent = "New Cell1";
 	let Cell2 = document.createElement("td");
-	cell2.textConent = "New Cell2";
+	cell2.textContent = "New Cell2";
 	newRow.appendChild(cell1);
 	newRow.appendChild(cell2);
